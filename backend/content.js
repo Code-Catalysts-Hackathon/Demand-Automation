635933f7493463994bdd083f28a25243a86b0ab5
siddharth <siddharth.gollapudi@gmail.com>
2024-07-26 11:15:30 +0530

    content.js

--- backend/content.js
+++ backend/content.js
@@ -1,6 +1,6 @@
 const demands = [
   {
-    id: 11,
+    id: "1",
     businessUnit: {id:1,name:"CL"},
     platform: {id:1,name:"Homes"},
     lab: {id:1,name:"Onboarding"},
@@ -16,13 +16,45 @@ const demands = [
     comments: "",
   },
   {
-    id: 1,
+    id: "2",
+    businessUnit: {id:1,name:"CL"},
+    platform: {id:1,name:"Homes"},
+    lab: {id:1,name:"Onboarding"},
+    featureTeam: {id:1,name:"Halifax"},
+    primarySkill: {id:1,name:"Java"},
+    secondarySkill: {id:2,name:"MySql"},
+    tertiarySkill: {id:3, name:"Playwright"},
+    grade: "D",
+    buHeadName: "John ",
+    department:"LTC HR",
+    demandDate: "",
+    status: "Pending",
+    comments: "",
+  },
+  {
+    id: "3",
     businessUnit: {id:1,name:"CL"},
     platform: {id:1,name:"Homes"},
     lab: {id:1,name:"Onboarding"},
     featureTeam: {id:1,name:"Avengers"},
     primarySkill: {id:1,name:"React"},
     secondarySkill: {id:2,name:"Node Js"},
+    tertiarySkill: {},
+    grade: "D",
+    buHeadName: "",
+    department:"Recruitment",
+    demandDate: "",
+    status: "Pending",
+    comments: "",
+  },
+  {
+    id: "4",
+    businessUnit: {id:1,name:"CL"},
+    platform: {id:1,name:"Homes"},
+    lab: {id:1,name:"Direct & Retention"},
+    featureTeam: {id:1,name:"MSP Simplification"},
+    primarySkill: {id:1,name:"MainFrame"},
+    secondarySkill: {id:2,name:"Db2"},
     tertiarySkill: {},
     grade: "E",
     buHeadName: "",
@@ -32,13 +64,13 @@ const demands = [
     comments: "",
   },
   {
-    id: 2,
-    businessUnit: {id:1,name:"CL"},
-    platform: {id:1,name:"Homes"},
-    lab: {id:1,name:"Onboarding"},
-    featureTeam: {id:1,name:"Avengers"},
-    primarySkill: {id:1,name:"React"},
-    secondarySkill: {id:2,name:"Node Js"},
+    id: "5",
+    businessUnit: {id:1,name:"BCB"},
+    platform: {id:1,name:"Investments"},
+    lab: {id:1,name:"Cards"},
+    featureTeam: {id:1,name:"Cards Team"},
+    primarySkill: {id:1,name:"Python"},
+    secondarySkill: {id:2,name:"Javascript"},
     tertiarySkill: {},
     grade: "E",
     buHeadName: "",
@@ -48,13 +80,45 @@ const demands = [
     comments: "",
   },
   {
-    id: 3,
-    businessUnit: {id:1,name:"CL"},
-    platform: {id:1,name:"Homes"},
-    lab: {id:1,name:"Onboarding"},
-    featureTeam: {id:1,name:"Avengers"},
-    primarySkill: {id:1,name:"React"},
-    secondarySkill: {id:2,name:"Node Js"},
+    id: "7",
+    businessUnit: {id:1,name:"BCB"},
+    platform: {id:1,name:"Enterprize"},
+    lab: {id:1,name:"Direct Channel"},
+    featureTeam: {id:1,name:"Feature team 1"},
+    primarySkill: {id:1,name:"DevOps"},
+    secondarySkill: {id:2,name:"Kubernetes"},
+    tertiarySkill: {},
+    grade: "F",
+    buHeadName: "",
+    department:"",
+    demandDate: "",
+    status: "Approved",
+    comments: "",
+  },
+  {
+    id: "8",
+    businessUnit: {id:1,name:"BCB"},
+    platform: {id:1,name:"Enterprize"},
+    lab: {id:1,name:"Broker Channel"},
+    featureTeam: {id:1,name:"Feature team 2"},
+    primarySkill: {id:1,name:"GCP"},
+    secondarySkill: {id:2,name:"Kubernetes"},
+    tertiarySkill: {},
+    grade: "F",
+    buHeadName: "",
+    department:"",
+    demandDate: "",
+    status: "Approved",
+    comments: "",
+  },
+  {
+    id: "9",
+    businessUnit: {id:1,name:"CR"},
+    platform: {id:1,name:"Consumer Team"},
+    lab: {id:1,name:"Brokers"},
+    featureTeam: {id:1,name:"Feature team 3"},
+    primarySkill: {id:1,name:"Azure"},
+    secondarySkill: {id:2,name:"Kubernetes"},
     tertiarySkill: {},
     grade: "E",
     buHeadName: "",
@@ -64,21 +128,22 @@ const demands = [
     comments: "",
   },
   {
-    id: 4,
-    businessUnit: {id:1,name:"CL"},
-    platform: {id:1,name:"Homes"},
-    lab: {id:1,name:"Onboarding"},
-    featureTeam: {id:1,name:"Avengers"},
-    primarySkill: {id:1,name:"React"},
-    secondarySkill: {id:2,name:"Node Js"},
+    id: "10",
+    businessUnit: {id:1,name:"BCB"},
+    platform: {id:1,name:"Platform 2"},
+    lab: {id:1,name:"Intermediary Channel"},
+    featureTeam: {id:1,name:"Feature team 5"},
+    primarySkill: {id:1,name:"AWS"},
+    secondarySkill: {id:2,name:"Docker"},
     tertiarySkill: {},
-    grade: "E",
+    grade: "F",
     buHeadName: "",
     department:"",
     demandDate: "",
     status: "Pending",
     comments: "",
   }
+
 ];
 
 const businessUnits = [
@@ -88,7 +153,7 @@ const businessUnits = [
   },
   {
     id: 1,
-    name: "Consumer Finance",
+    name: "Consumer Relationships",
   },
   {
     id: 2,
@@ -100,13 +165,65 @@ const businessUnits = [
   },
   {
     id: 4,
-    name: "HR dept",
+    name: "Business And Commercial Bankking",
   },
 ];
 
+const employees = [
+  {
+    "employeeId": "5608247",
+    "name": "Krishna",
+    "email": "Krishna@lbg.com",
+    "mobile": "9966040003",
+    "grade": "F",
+    "JobTitle": "Lead Software Engineer"
+  },
+  {
+    "employeeId": "5608111",
+    "name": "Gautam",
+    "email": "Gautam@lbg.com",
+    "mobile": "9966040111",
+    "grade": "E",
+    "JobTitle": "Senior Software Engineer"
+  },
+  {
+    "employeeId": "5608112",
+    "name": "David",
+    "email": "David@lbg.com",
+    "mobile": "9966040112",
+    "grade": "E",
+    "jobTitle": "Senior Software Engineer"
+  },
+  {
+    "employeeId": "5608113",
+    "name": "Jenny",
+    "email": "Jenny@lbg.com",
+    "mobile": "9966040113",
+    "grade": "D",
+    "jobTitle": "Software Engineer"
+  },
+  {
+    "employeeId": "5608114",
+    "name": "John",
+    "email": "John@Lbg.com",
+    "mobile": "9966040114",
+    "grade": "D",
+    "JobTitle": "Software Engineer"
+  },
+  {
+    "employeeId": "5608115",
+    "name": "Prakash",
+    "email": "Prakash@Lbg.com",
+    "mobile": "9966040114",
+    "grade": "E",
+    "JobTitle": "Senior Quality Engineer"
+  }
+]
+
 module.exports = {
   demands,
-  businessUnits
+  businessUnits,
+  employees
 };
 
 
